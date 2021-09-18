c8d46c95c0fafb6c92d87850949bcf14c5030f62
Alexey Burlakov <feraturdev@gmail.com>
2021-09-18 20:26:10 +0300

    Add Item 39

---
+++ 5_classes_and_interfaces/39_classmethod.py
@@ -0,0 +1,134 @@
+# Item 39: Use @classmethod Polymorphism to Construct Objects Generically
+
+# In Python, not only do objects support polymorphism, but classes do
+# as well.
+class InputData:
+    def read(self):
+        raise NotImplementedError
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+class Worker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+    def map(self):
+        raise NotImplementedError
+    def reduce(self, other):
+        raise NotImplementedError
+class LineCountWorker(Worker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+    def reduce(self, other):
+        self.result += other.result
+
+import os
+def generate_inputs(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+from threading import Thread
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+    first, *rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.results
+
+def mapreduce(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+import os
+import random
+def write_test_files(tmpdir):
+    os.makedirs(tmpdir)
+    for i in range(100):
+        with open(os.path.join(tmpdir, str(i)), 'w') as f:
+            f.write('\n' * random.randint(0, 100))
+
+tmpdir = 'test_inputs'
+# write_test_files(tmpdir)
+# result = mapreduce(tmpdir)
+# print(f'There are {result} lines')
+# There are 4360 lines
+
+# This problem boils down to needing a generic way to construct objects.
+# In other languages, you’d solve this problem with constructor polymorphism,
+# requiring that each InputData subclass provides a special constructor that
+# can be used generically by the helper methods
+# that orchestrate the MapReduce (similar to the factory pattern). The
+# trouble is that Python only allows for the single constructor method
+# __init__. It’s unreasonable to require every InputData subclass to
+# have a compatible constructor.
+# The best way to solve this problem is with class method polymorphism.
+class GenericInputData:
+    def read(self):
+        raise NotImplementedError
+
+    @classmethod
+    def generate_inputs(cls, config):
+        raise NotImplementedError
+
+class PathInputData(GenericInputData):
+    def __init__(self, path):
+            super().__init__()
+            self.path = path
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config['data_dir']
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+class GenericWorker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+    def map(self):
+        raise NotImplementedError
+    def reduce(self, other):
+        raise NotImplementedError
+    @classmethod
+    def create_workers(cls, input_class, config):
+        workers = []
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+class LineCountWorker(GenericWorker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+    def reduce(self, other):
+        self.result += other.result
+
+def mapreduce(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+# config = {'data_dir': tmpdir}
+# result = mapreduce(LineCountWorker, PathInputData, config)
+# print(f'There are {result} lines')
+# There are 4360 lines
+
+# ✦ Python only supports a single constructor per class: the __init__
+# method.
+# ✦ Use @classmethod to define alternative constructors for your classes.
+# ✦ Use class method polymorphism to provide generic ways to build
+# and connect many concrete subclasses.
